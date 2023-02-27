4c0d05fccb3c63c953e7700af4513ce330aaf55b
vinicius-sabadim <vinicius.sabadim@gmail.com>
2023-02-27 21:21:09 +0100

    add mouse event listeners

--- script.js
+++ script.js
@@ -36,6 +36,32 @@ window.addEventListener("load", () => {
       this.width = this.canvas.width
       this.height = this.canvas.height
       this.player = new Player(this)
+      this.mouse = {
+        x: this.width * 0.5,
+        y: this.height * 0.5,
+        pressed: false,
+      }
+
+      this.canvas.addEventListener("mousedown", (e) => {
+        this.mouse = {
+          x: e.offsetX,
+          y: e.offsetY,
+          pressed: true,
+        }
+      })
+
+      this.canvas.addEventListener("mouseup", (e) => {
+        this.mouse = {
+          x: e.offsetX,
+          y: e.offsetY,
+          pressed: false,
+        }
+      })
+
+      this.canvas.addEventListener("mousemove", (e) => {
+        this.mouse.x = e.offsetX
+        this.mouse.y = e.offsetY
+      })
     }
 
     render(context) {
