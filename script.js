3a3dc9d430fa7b3ec0c913df11cda2fe091b0ccb
vinicius-sabadim <vinicius.sabadim@gmail.com>
2023-02-27 20:16:40 +0100

    initial setup of the canvas

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas1")
+  const ctx = canvas.getContext("2d")
+
+  // Set canvas size
+  canvas.width = 1280
+  canvas.height = 720
+})
