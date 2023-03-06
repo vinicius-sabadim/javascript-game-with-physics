b58dfee26c1e334894eb209a72c308bbcfb8d4bd
vinicius-sabadim <vinicius.sabadim@gmail.com>
2023-03-06 21:41:27 +0100

    Draw the obstacles using a sprite image

--- script.js
+++ script.js
@@ -17,9 +17,19 @@ window.addEventListener("load", () => {
       this.collisionX = Math.random() * this.game.width
       this.collisionY = Math.random() * this.game.height
       this.collisionRadius = 60
+      this.image = document.getElementById('obstacles')
+      this.spriteWidth = 250
+      this.spriteHeight = 250
+      this.width = this.spriteWidth
+      this.height = this.spriteHeight
+      this.spriteX = this.collisionX - this.width * 0.5
+      this.spriteY = this.collisionY - this.height * 0.5 - 70
     }
 
     draw(context) {
+      // Draw the obstacle image
+      context.drawImage(this.image, 0, 0, this.width, this.height, this.spriteX, this.spriteY, this.width, this.height)
+
       // Draw the circle
       context.beginPath()
       context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2)
